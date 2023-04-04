15026dc48e6eb903a17adf9613a19f1bb4c075e8
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-04-03 21:53:34 -0400

    Added responsive menu navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
@@ -7,15 +7,35 @@ import {
   AppBar,
   Box,
   Button,
+  Drawer,
   Container,
   IconButton,
   Menu,
   MenuItem,
   Toolbar,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
+import { useTheme } from "@mui/system";
+// import MenuIcon from "@mui/icons-material/Menu";
 
-function NavBar() {
+const NavBar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setDrawerOpen(open);
+    };
+
   const buttonSX = {
     textTransform: "none",
     color: "#000",
@@ -36,7 +56,61 @@ function NavBar() {
     "&:hover::after": {
       height: "6px",
     },
+    [theme.breakpoints.down("sm")]: {
+      "&::after": {
+        bottom: "0px",
+      },
+    },
   };
+
+  const drawerList = (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.9rem",
+        py: "3rem",
+        width: "100vw",
+      }}
+    >
+      <Link href="/" passHref>
+        <Button sx={buttonSX}>Home</Button>
+      </Link>
+      <Link href="/about" passHref>
+        <Button sx={buttonSX}>About</Button>
+      </Link>
+      <Link href="/contact" passHref>
+        <Button sx={buttonSX}>Contact</Button>
+      </Link>
+      <Link href="/blog" passHref>
+        <Button sx={buttonSX}>Blog</Button>
+      </Link>
+      <Link href="/careers" passHref>
+        <Button sx={buttonSX}>Careers</Button>
+      </Link>
+      {/* Request Invite Button */}
+      <Button
+        sx={{
+          background: "linear-gradient(90deg, #33d167 0%, #2bb9cd 100%)",
+          color: "#fff",
+          borderRadius: "50px",
+          py: "0.7rem",
+          px: "2.2rem",
+          fontSize: "0.8rem",
+          fontWeight: 600,
+          textTransform: "none",
+          fontFamily: "Inter, sans-serif",
+          "&:hover": {
+            opacity: 0.7,
+          },
+        }}
+      >
+        Request Invite
+      </Button>
+    </Box>
+  );
 
   return (
     <>
@@ -54,53 +128,84 @@ function NavBar() {
           </Link>
 
           {/* Menu Items */}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-around",
-              gap: "0.9rem",
-            }}
-          >
-            <Link href="/" passHref>
-              <Button sx={buttonSX}>Home</Button>
-            </Link>
-            <Link href="/about" passHref>
-              <Button sx={buttonSX}>About</Button>
-            </Link>
-            <Link href="/contact" passHref>
-              <Button sx={buttonSX}>Contact</Button>
-            </Link>
-            <Link href="/blog" passHref>
-              <Button sx={buttonSX}>Blog</Button>
-            </Link>
-            <Link href="/careers" passHref>
-              <Button sx={buttonSX}>Careers</Button>
-            </Link>
-          </Box>
-
-          {/* Request Invite Button */}
-          <Button
-            sx={{
-              background: "linear-gradient(90deg, #33d167 0%, #2bb9cd 100%)",
-              color: "#fff",
-              borderRadius: "50px",
-              py: "0.7rem",
-              px: "2.2rem",
-              fontSize: "0.8rem",
-              fontWeight: 600,
-              textTransform: "none",
-              fontFamily: "Inter, sans-serif",
-              "&:hover": {
-                opacity: 0.7,
-              },
-            }}
-          >
-            Request Invite
-          </Button>
+          {isMobile ? (
+            <>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer(!drawerOpen)}
+              >
+                <Image
+                  src="/icon-hamburger.svg"
+                  alt="hamburger menu"
+                  width={24}
+                  height={24}
+                />{" "}
+                {/* Updated line */}
+              </IconButton>
+              <Drawer
+                anchor="top"
+                open={drawerOpen}
+                onClose={toggleDrawer(false)}
+                sx={{ zIndex: (theme.zIndex?.appBar || 1000) + 1 }} // Update this line
+              >
+                {drawerList}
+              </Drawer>
+            </>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  gap: "0.9rem",
+                }}
+              >
+                <Link href="/" passHref>
+                  <Button sx={buttonSX}>Home</Button>
+                </Link>
+                <Link href="/about" passHref>
+                  <Button sx={buttonSX}>About</Button>
+                </Link>
+                <Link href="/contact" passHref>
+                  <Button sx={buttonSX}>Contact</Button>
+                </Link>
+                <Link href="/blog" passHref>
+                  <Button sx={buttonSX}>Blog</Button>
+                </Link>
+                <Link href="/careers" passHref>
+                  <Button sx={buttonSX}>Careers</Button>
+                </Link>
+                {/* Request Invite Button */}
+              </Box>
+              <Box>
+                <Button
+                  sx={{
+                    background:
+                      "linear-gradient(90deg, #33d167 0%, #2bb9cd 100%)",
+                    color: "#fff",
+                    borderRadius: "50px",
+                    py: "0.7rem",
+                    px: "2.2rem",
+                    fontSize: "0.8rem",
+                    fontWeight: 600,
+                    textTransform: "none",
+                    fontFamily: "Inter, sans-serif",
+                    "&:hover": {
+                      opacity: 0.7,
+                    },
+                  }}
+                >
+                  Request Invite
+                </Button>
+              </Box>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </>
   );
-}
+};
 
 export default NavBar;
